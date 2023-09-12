90be095baae5412c6955054936a1b9db7154cf48
jjppddDeveloper <jjppdd.userrr@gmail.com>
2023-09-12 22:56:48 +0900

    APIによるGet処理

---
+++ src/components/Utils.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+
+const Utils = () => {
+  // useState：const [状態, 状態更新関数] = useState(初期値);
+  const [posts, setPosts] = useState([]);
+
+  // useEffect：コンポーネントのライフサイクル（componentDidMount, componentDidUpdate, componentWillUnmount）のタイミングで実行される
+  /*
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts", { method: "GET" })
+      .then((res) => res.json())
+      .then((data) => {
+        setPosts(data);
+      });
+  }, []);
+  */
+
+  function getData() {
+    fetch("https://jsonplaceholder.typicode.com/posts", { method: "GET" })
+      .then((res) => res.json())
+      .then((data) => {
+        setPosts(data);
+      });
+  }
+
+  return (
+    <div>
+      <button onClick={() => getData()}>API</button>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Utils;
